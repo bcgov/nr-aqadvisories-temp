3e7323106ff179066518d2474939942f453b57d0
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-07-25 15:15:30 -0700

    workflow update

--- air-quality-statements/astro.config.mjs
+++ air-quality-statements/astro.config.mjs
@@ -4,6 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.BASE_URL,
+  base: process.env.BASE_URL || '.',
   integrations: [react()]
 });
